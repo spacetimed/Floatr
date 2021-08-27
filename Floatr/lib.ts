7b74adfdface910ce19427206fe68a08cf496f54
FFFFFF <s127813127@gmail.com>
2021-08-26 18:57:40 -0700

    Library foundation

--- Floatr/lib.ts
+++ Floatr/lib.ts
@@ -1,7 +1,39 @@
+interface FloatrSettings {
+    width: any;
+    height: any;
+    count: number;
+}
+
+interface FloatrParticle {
+    x: number;
+    y: number;
+    variance: number;
+    size: number;
+}
+
+interface FloatrAllParticles extends Array<FloatrParticle> {}
+
 class Floatr {
-    constructor(container, settings) {
-        this.spawned = false;
+
+    private __t: number; //Global Timer
+    private __g: number; //Gravity
+
+    private container: HTMLElement;
+    private settings: FloatrSettings;
+    private width: any;
+    private height: any;
+    private spawned: boolean = false;
+
+    private __singleParticle: FloatrParticle;
+    private particles: FloatrAllParticles;
+
+    public floatrCanvas: HTMLCanvasElement;
+    public floatrCanvasCtx: CanvasRenderingContext2D;
+
+    constructor(container: string, settings: FloatrSettings) {
+        
         this.showBoot();
+
         this.container = document.getElementById(container);
         this.settings = settings;
         this.width = (this.settings.width == 'inherit') ? this.container.offsetWidth : this.settings.width;
@@ -9,14 +41,18 @@ class Floatr {
         this.__t = 0;
         this.particles = [];
         this.createCanvasElement();
+
     }
-    logger(message) {
+
+    private logger(message: string) {
         console.log('%c<floatr>%c ' + message + '', 'color:DarkBlue;background-color:#fff;font-weight:bold;', 'color:#fff;');
     }
-    showBoot() {
+
+    private showBoot() {
         this.logger('initializing...');
     }
-    createCanvasElement() {
+
+    private createCanvasElement() {
         this.floatrCanvas = document.createElement('canvas');
         this.floatrCanvas.id = 'floatrCanvas';
         this.floatrCanvas.width = this.width;
@@ -26,32 +62,40 @@ class Floatr {
         this.container.appendChild(this.floatrCanvas);
         this.logger('canvas element created');
     }
-    Render() {
+
+    public Render() { //begin loop
         this.__animationLoop();
     }
-    __animationLoop(timestamp = 0) {
+
+    private __animationLoop(timestamp: Number = 0) {
         this.__t++;
-        if (!this.spawned) {
-            for (let i = 0; i < this.settings.count; i++) {
+
+        if(!this.spawned) {
+            for(let i = 0; i < this.settings.count; i++) {
                 this.__singleParticle = {
-                    'x': Math.floor(Math.random() * (this.width + 1)),
-                    'y': Math.floor(Math.random() * (this.height + 500)) - 500,
-                    'variance': Math.floor(Math.random() * 5) + 1,
-                    'size': parseFloat(Math.random().toPrecision(2)) + 1
-                };
+                    'x' : Math.floor(Math.random() * (this.width + 1)),
+                    'y' :  Math.floor(Math.random() * (this.height + 500)) - 500,
+                    'variance' : Math.floor(Math.random() * 5) + 1,
+                    'size' : parseFloat(Math.random().toPrecision(2)) + 1
+                }
                 this.particles[i] = this.__singleParticle;
             }
-            this.__g = 0.05;
+            this.__g =  0.05;
             this.spawned = true;
         }
-        for (let i = 0; i < this.settings.count; i++) {
+        for(let i = 0; i < this.settings.count; i++)
+        {
             //let calcT = Math.sin(this.__t * this.particles[i].variance) * 10;
             //this.particles[i].y += (1/2) * this.__g * calcT * this.particles[i].variance;
+
             var circle = new Path2D();
             circle.arc(this.particles[i].x, this.particles[i].y, 20, 0, 2 * Math.PI);
             this.floatrCanvasCtx.fillStyle = 'rgba(255, 255, 255, 0.5)';
             this.floatrCanvasCtx.fill(circle);
+
         }
+
         window.requestAnimationFrame((timestamp) => this.__animationLoop(timestamp));
     }
+
 }
